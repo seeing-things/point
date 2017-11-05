d24d5cdd5f0dd5e2579b020382cdf761c66511bf
Brett Gottula <bgottula@gmail.com>
2017-11-04 23:01:21 -0700

    Changes version number to 0.1, adds minimum version spec to pyserial dependency

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.0',
+    version='0.1.0',
 
     description='Serial command interfaces for Celestron telescopes',
     long_description=long_description,
@@ -41,6 +41,6 @@ setup(
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
 
     install_requires=[
-        'pyserial',
+        'pyserial>=3.0',
     ],
 )
