96c1fcf8269b14ac0629d6bac9a51e4b833c0757
Justin Gottula <justin@jgottula.com>
2018-05-06 19:34:00 -0700

    Add reminders to reimplement not-yet-reimplemented G2 commands

--- point/gemini.py
+++ point/gemini.py
@@ -86,34 +86,42 @@ class Gemini2(object):
         """Test command. Should return the same character as the argument."""
         return self.exec_cmd(G2Cmd_Echo(char)).get()
 
+    # TODO: reimplement this
 #    def align_to_object(self):
 #        """Add selected object to pointing model."""
 #        return self.lx200_cmd('Cm', expect_reply=True)
 
+    # TODO: reimplement this
 #    def sync_to_object(self):
 #        """Synchronize to selected object."""
 #        return self.lx200_cmd('CM', expect_reply=True)
 
+    # TODO: reimplement this
 #    def select_pointing_model(self, num):
 #        """Select a pointing model (0 or 1)."""
 #        return int(self.lx200_cmd('C' + chr(num), expect_reply=True))
 
+    # TODO: reimplement this
 #    def select_pointing_model_for_io(self):
 #        """Selects the active pointing model for I/O access."""
 #        return int(self.lx200_cmd('Cc', expect_reply=True))
 
+    # TODO: reimplement this
 #    def get_pointing_model(self):
 #        """Get number of active pointing model (0 or 1)."""
 #        return int(self.lx200_cmd('C?', expect_reply=True))
 
+    # TODO: reimplement this
 #    def init_align(self):
 #        """Perform Initial Align with selected object."""
 #        return self.lx200_cmd('CI', expect_reply=True)
 
+    # TODO: reimplement this
 #    def reset_model(self):
 #        """Resets the currently selected model."""
 #        return int(self.lx200_cmd('CR', expect_reply=True))
 
+    # TODO: reimplement this
 #    def reset_last_align(self):
 #        """Resets the last alignment of currently selected model."""
 #        return int(self.lx200_cmd('CU', expect_reply=True))
@@ -121,65 +129,83 @@ class Gemini2(object):
 
     ### Focus Control Commands
 
+    # TODO: reimplement this
 #    def focus_in(self):
 #        self.lx200_cmd('F+')
 
+    # TODO: reimplement this
 #    def focus_out(self):
 #        self.lx200_cmd('F-')
 
+    # TODO: reimplement this
 #    def focus_stop(self):
 #        self.lx200_cmd('FQ')
 
+    # TODO: reimplement this
 #    def focus_fast(self):
 #        self.lx200_cmd('FF')
 
+    # TODO: reimplement this
 #    def focus_medium(self):
 #        self.lx200_cmd('FM')
 
+    # TODO: reimplement this
 #    def focus_slow(self):
 #        self.lx200_cmd('FS')
 
 
     ### Get Information Commands
 
+    # TODO: reimplement this
 #    def get_alt(self):
 #        """Altitude in signed degrees format [-90.0, +90.0]."""
 #        return float(self.lx200_cmd('GA', expect_reply=True))
 
+    # TODO: reimplement this
 #    def get_led_brightness(self):
 #        return int(self.lx200_cmd('GB', expect_reply=True))
 
+    # TODO: reimplement this
 #    def get_local_date(self):
 #        """Date as a string in mm/dd/yy format."""
 #        return self.lx200_cmd('GC', expect_reply=True)
 
+    # TODO: reimplement this
 #    def get_clock_format(self):
 #        return self.lx200_cmd('Gc', expect_reply=True)
 
+    # TODO: reimplement this
 #    def get_dec(self):
 #        """Apparent declination in signed degrees [-90.0,+90.0]."""
 #        return float(self.lx200_cmd('GD', expect_reply=True))
 
+    # TODO: reimplement this
 #    def get_obj_dec(self):
 #        """Selected object's declination in signed degrees."""
 #        return float(self.lx200_cmd('Gd', expect_reply=True))
 
+    # TODO: reimplement this
 #    def get_alarm_time(self):
 #        return self.lx200_cmd('GE', expect_reply=True)
 
+    # TODO: reimplement this
 #    def get_utc_offset(self):
 #        return self.lx200_cmd('GG', expect_reply=True)
 
+    # TODO: reimplement this
 #    def get_site_lon(self):
 #        return float(self.lx200_cmd('Gg', expect_reply=True))
 
+    # TODO: reimplement this
 #    def get_hour_angle(self):
 #        """Hour angle in signed degrees."""
 #        return float(self.lx200_cmd('GH', expect_reply=True))
 
+    # TODO: reimplement this
 #    def get_info_buffer(self):
 #        return self.lx200_cmd('GI', expect_reply=True)
 
+    # TODO: reimplement this
 #    def get_local_time(self):
 #        """Local time in hours as a float."""
 #
@@ -187,56 +213,71 @@ class Gemini2(object):
 #        # worse than one second. High precision format has better resolution.
 #        return float(self.lx200_cmd('GL', expect_reply=True))
 
+    # TODO: reimplement this
 #    def get_meridian_side(self):
 #        """Returns 'E' or 'W' to indicate side of meridian."""
 #        return self.lx200_cmd('Gm', expect_reply=True)
 
+    # TODO: reimplement this
 #    def get_site_name(self, site_num=0):
 #        site_letters = ['M', 'N', 'O', 'P']
 #        return self.lx200_cmd('G' + site_letters[site_num], expect_reply=True)
 
+    # TODO: reimplement this
 #    def get_ra(self):
 #        """Apparent right ascension in hours [0.0,24.0)."""
 #        return float(self.lx200_cmd('GR', expect_reply=True))
 
+    # TODO: reimplement this
 #    def get_obj_ra(self):
 #        """Selected object's right ascension in signed degrees."""
 #        return float(self.lx200_cmd('Gr', expect_reply=True))
 
+    # TODO: reimplement this
 #    def get_sidereal_time(self):
 #        return float(self.lx200_cmd('GS', expect_reply=True))
 
+    # TODO: reimplement this
 #    def get_site_lat(self):
 #        return float(self.lx200_cmd('Gt', expect_reply=True))
 
     # Omitting command 'GV' which is redundant with 'GVN'.
 
+    # TODO: reimplement this
 #    def get_software_build_date(self):
 #        return self.lx200_cmd('GVD', expect_reply=True)
 
+    # TODO: reimplement this
 #    def get_software_level(self):
 #        return self.lx200_cmd('GVN', expect_reply=True)
 
+    # TODO: reimplement this
 #    def get_product_string(self):
 #        return self.lx200_cmd('GVP', expect_reply=True)
 
+    # TODO: reimplement this
 #    def get_software_build_time(self):
 #        return self.lx200_cmd('GVT', expect_reply=True)
 
+    # TODO: reimplement this
 #    def get_max_velocity(self):
 #        """Maximum velocity of both axes."""
 #        return self.lx200_cmd('Gv', expect_reply=True, reply_len=1)
 
+    # TODO: reimplement this
 #    def get_velocity_ra(self):
 #        return self.lx200_cmd('GW', expect_reply=True, reply_len=1)
 
+    # TODO: reimplement this
 #    def get_velocity_dec(self):
 #        return self.lx200_cmd('Gw', expect_reply=True, reply_len=1)
 
+    # TODO: reimplement this
 #    def get_velocity(self):
 #        """Velocity of both axes: RA, DEC (2 characters)."""
 #        return self.lx200_cmd('Gu', expect_reply=True, reply_len=2)
 
+    # TODO: reimplement this
 #    def get_az(self):
 #        """Azimuth in signed degrees format [0.0, 360.0)."""
 #        return float(self.lx200_cmd('GZ', expect_reply=True))
@@ -244,55 +285,69 @@ class Gemini2(object):
 
     ### Park Commands
 
+    # TODO: reimplement this
 #    def park_home(self):
 #        self.lx200_cmd('hP')
 
+    # TODO: reimplement this
 #    def park_startup(self):
 #        self.lx200_cmd('hC')
 
+    # TODO: reimplement this
 #    def park_zenith(self):
 #        self.lx200_cmd('hZ')
 
+    # TODO: reimplement this
 #    def sleep(self):
 #        self.lx200_cmd('hN')
 
+    # TODO: reimplement this
 #    def wake(self):
 #        self.lx200_cmd('hW')
 
 
     ### Move Commands
 
+    # TODO: reimplement this
 #    def goto_object_horiz(self):
 #        """Goto object selected with horizontal coordinates."""
 #        return self.lx200_cmd('MA', expect_reply=True)
 
+    # TODO: reimplement this
 #    def search_pattern(self, arcmins):
 #        """Move at find speed in a meander search pattern."""
 #        self.lx200_cmd('MF' + str(arcmins), expect_reply=True)
 
+    # TODO: reimplement this
 #    def move_lock(self):
 #        self.lx200_cmd('ML', expect_reply=True)
 
+    # TODO: reimplement this
 #    def move_unlock(self):
 #        self.lx200_cmd('Ml', expect_reply=True)
 
+    # TODO: reimplement this
 #    def meridian_flip(self):
 #        return self.lx200_cmd('Mf', expect_reply=True)
 
+    # TODO: reimplement this
 #    def goto_object(self, allow_meridian_flip=False):
 #        """Goto object selected from database or equatorial coordinates."""
 #        cmd = 'MM' if allow_meridian_flip else 'MS'
 #        return lx200_cmd(cmd, expect_reply=True)
 
+    # TODO: reimplement this
 #    def move(self, direction):
 #        """Move in a direction: 'east', 'west', 'north', or 'south'."""
 #        if direction not in ['east', 'west', 'north', 'south']:
 #            raise ValueError('invalid direction for move command')
 #        self.lx200_cmd('M' + direction[0])
 
+    # TODO: reimplement this
 #    def move_ticks(self, ra_steps, dec_steps):
 #        self.lx200_cmd('mi' + str(ra_steps) + ';' + str(dec_steps))
 
+    # TODO: reimplement this
 #    def set_step_multiplier(self, multiplier):
 #        self.lx200_cmd('mm' + str(multiplier))
 
